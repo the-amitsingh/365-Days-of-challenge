5ee3ab37c8e079728f51437f9923cb586d302d56
Amit kumar singh <amitsinghdbg2001@gmail.com>
2022-02-04 20:12:13 +0530

    Creating Custom Modules in Node Using NodeJs

---
+++ day51to60/day57/index.js
@@ -0,0 +1,7 @@
+// const average = require("./mode.js");
+// console.log(average([3,4]) );
+const mode= require("./mode.js");
+console.log(mode.name);
+// console.log(mode.avg([3,4]) );
+
+console.log("This is index.js");
