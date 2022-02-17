7cee8c48357d18d8272613060cc5df85fd0721c3
Amit kumar singh <amitsinghdbg2001@gmail.com>
2022-02-17 11:29:03 +0530

    Object Literals, Constructors and Object Oriented JavaScript.

---
+++ day61to70/day70/day70_1.js
@@ -0,0 +1,34 @@
+console.log("Object Literals, Constructors and Object Oriented JavaScript");
+
+// Object Literal for creating objects
+let car = {
+  name: "Maruti 800",
+  topSpeed: 89,
+  run: function() {
+    console.log("car is running");
+  }
+};
+// we have already seen constructors like these:
+// new Date();
+
+// Creating a constructor for cars
+function GeneralCar(givenName, givenSpeed) {
+  this.name = givenName;
+  this.topSpeed = givenSpeed;
+  this.run = function() {
+    console.log(`${this.name} Is Running`);
+  };
+  this.analyze = function() {
+    console.log(
+      `This car is slower by ${200 - this.topSpeed} Km/H than Mercedes`
+    );
+  };
+}
+car1 = new GeneralCar("Nissan", 180);
+car2 = new GeneralCar("Marutu Alto", 80);
+car3 = new GeneralCar("Mercedes", 200);
+console.log(car1, car2, car3);
+
+
+
+ 
