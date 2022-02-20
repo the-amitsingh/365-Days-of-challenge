52a3e346e92a770c2e1fcae6e21e45fada91ec17
Amit kumar singh <amitsinghdbg2001@gmail.com>
2022-02-20 21:29:41 +0530

    ES6 Classes and Inheritance in JS.

---
+++ day71to80/day73/day73.js
@@ -0,0 +1,52 @@
+console.log("this is Tutorial31.js");
+
+class Employee {
+    constructor(givenName, givenExperience, givenDivision) {
+        this.name = givenName;
+        this.experience = givenExperience;
+        this.division = givenDivision;
+    }
+
+    slogan(){
+        return `I am ${this.name} and this company is the best`;
+    }
+
+    joiningYear(){
+        return 2020 - this.experience;
+    }
+
+    static add(a, b){
+        return a + b;
+    }
+}
+
+class Programmer extends Employee{
+    constructor(givenName, givenExperience, givenDivision, language, github){
+        super(givenName, givenExperience, givenDivision);
+        this.language = language;
+        this.github = github;
+    }
+
+    favoriteLanguage(){
+        if (this.language == 'python'){
+            return 'Python';
+        }
+        else{
+            return 'JavaScript';
+        }
+    }
+
+    static multiply(a, b){
+        return a * b;
+    }
+}
+
+// harry = new Employee("Harry", 56, "Division");
+// console.log(harry.joiningYear());
+// console.log(Employee.add(34, 5))
+rohan = new Programmer("Rohan", 3, "Lays", "Go", "Rohan420")
+console.log(rohan)
+console.log(rohan.favoriteLanguage())
+console.log(Programmer.multiply(5, 7));
+
+
