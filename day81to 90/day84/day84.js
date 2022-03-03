a63c0cfe434186ab6b06bb383cb12003649bc8d4
Amit kumar singh <amitsinghdbg2001@gmail.com>
2022-03-03 23:36:31 +0530

    Error Handling & Try Catch in Javascript

---
+++ day81to 90/day84/day84.js
@@ -0,0 +1,28 @@
+console.log("This is tutorial 44");
+
+// Pretend this is coming from a server as response
+let a = "Amit  bhai ";
+a = undefined;
+if (a !=undefined){
+    throw new Error('This is not undefined');
+}
+else{
+    console.log('This is undefined');
+}
+
+
+try {
+    null.console
+    console.log("We are inside try block");
+    
+    functionHarry();
+    
+} catch(error) {
+    console.log(error)
+    console.log("Are you okay?");
+    console.log(error.name);
+    console.log(error.message);
+    
+} finally {
+    console.log("Finally we will run this")
+}
