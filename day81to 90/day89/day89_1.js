c4251fac42c69ddb29b25f7cd4299a766e159bbd
Amit kumar singh <amitsinghdbg2001@gmail.com>
2022-03-08 23:33:36 +0530

    alarm app

--- day81to 90/day89/day89_1.js
+++ day81to 90/day89/day89_1.js
@@ -2,5 +2,7 @@ console.log('alarm app');
 // You have to crate an alarm clock in javascript (Use your creativity)
 // Allow user to set alarm for a certain time
 
+
+
 var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
 audio.play();
