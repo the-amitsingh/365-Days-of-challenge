6db7fe98b16bf17ec35f6ff4a9ff1d7313c1e2c4
Amit kumar singh <amitsinghdbg2001@gmail.com>
2022-03-11 23:32:24 +0530

    Smart Page Crawler Exercise In JS

---
+++ day91to100/day92/day92.js
@@ -0,0 +1,10 @@
+let str = "python";
+let links = document.links;
+console.log(links);
+let href;
+Array.from(links).forEach(function(element) {
+  href = element.href;
+  if (href.includes(str)) {
+    console.log(href);
+  }
+});
