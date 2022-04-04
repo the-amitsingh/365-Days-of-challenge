ca4fac38cdc168f9f6505d9c3345c21b8c4ede39
Amit kumar singh <amitsinghdbg2001@gmail.com>
2022-04-04 22:16:56 +0530

    MongoDb Tutorial: Searching/Querying data from the Mongo Database

--- day111to120/day115/day115.js
+++ day111to120/day115/day115.js
@@ -1,6 +1,6 @@
 // inserting data in mongo
-db
 use AMitKart
+db
 db.items.insertOne({Name:"Samsung 30s",price:28000,rating:4.6,quantity:450,sold:103})
 
 db.items.insertMany([{Name:"Samsung 30s",price:28000,rating:4.6,quantity:450,sold:103},{Name:"Moto 20+",price:22000,rating:3.6,quantity:450,sold:103},{Name:"iphone 20+",price:220000,rating:4.8,quantity:450,sold:206}])
