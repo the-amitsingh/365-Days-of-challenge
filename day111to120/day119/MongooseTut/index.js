b9799014d25f3ac37cb839e8005d752cf31c1867
Amit kumar singh <amitsinghdbg2001@gmail.com>
2022-04-08 23:48:49 +0530

    MongoDb : Using Mongoose in NodeJs

--- day111to120/day119/MongooseTut/index.js
+++ day111to120/day119/MongooseTut/index.js
@@ -1,9 +1,28 @@
 // getting-started.js
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
 
 var db = mongoose.connection;
-db.on('error',console.error.bind(console,'connection error:'));
-db.once('open',function(){
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
     console.log("we are connected ");
 })
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+
+kittySchema.methods.speak = function speak() {
+    var greeting =  "My name is " + this.name   
+    console.log(greeting);
+};
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var Amitkitty = new Kitten({ name: 'Amitkitty ' });
+// console.log(Amitkitty.name);
+// Amitkitty.speak();
+
+Amitkitty.save(function (err, Amitkitty) {
+    if (err) return console.error(err);
+    Amitkitty.speak();
+  });
